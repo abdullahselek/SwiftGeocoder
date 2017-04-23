ca7ace3a694ff1939ef0fdb3c29784eec18ba8e6
abdullahselek <abdullahselek@gmail.com>
2017-04-23 09:23:29 +0200

    create project

---
+++ SwiftGeocoder/SwiftGeocoder.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGeocoder.h
+//  SwiftGeocoder
+//
+//  Created by Abdullah Selek on 23/04/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftGeocoder.
+FOUNDATION_EXPORT double SwiftGeocoderVersionNumber;
+
+//! Project version string for SwiftGeocoder.
+FOUNDATION_EXPORT const unsigned char SwiftGeocoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGeocoder/PublicHeader.h>
+
+
